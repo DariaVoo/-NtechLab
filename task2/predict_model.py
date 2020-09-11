00321a93178ec13f16c3043fc2ee9108e20a06f0
DariaVoo <dasha2003.voo@yandex.ru>
2020-09-11 18:45:45 +0000

    done

--- task2/predict_model.py
+++ task2/predict_model.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import LabelEncoder
 from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
 
+import sys
 from pathlib import Path
 import pandas as pd
 from GenderDataset import GenderDataset
@@ -30,41 +31,40 @@ def predict(model, test_loader):
     probs = nn.functional.softmax(torch.cat(logits), dim=-1).numpy()
     return probs
 
+if __name__ == "__main__":
+  # Будем предсказывать на GPU
+  train_on_gpu = torch.cuda.is_available()
+  if not train_on_gpu:
+      print('CUDA is not available.  Training on CPU ...')
+  else:
+      print('CUDA is available!  Training on GPU ...')
+  DEVICE = torch.device("cuda")
 
-# Будем предсказывать на GPU
-train_on_gpu = torch.cuda.is_available()
-if not train_on_gpu:
-    print('CUDA is not available.  Training on CPU ...')
-else:
-    print('CUDA is available!  Training on GPU ...')
-DEVICE = torch.device("cuda")
+  # ВАЖНО!!! Укажите путь до модели
+  PATH_MODEL = 'gdrive/My Drive/NtechLab_test/modelext.pth'
+  model = torch.load(PATH_MODEL)
 
-# ВАЖНО!!! Укажите путь до модели
-PATH = 'gdrive/My Drive/NtechLab_test/modelext.pth'
-model = torch.load(PATH)
+  PATH_DATA = Path(sys.argv[1])
+  data_files = list(PATH_DATA.rglob('*.jpg'))
 
-# PATH_DATA = Path(input())
-PATH_DATA = Path('gdrive/My Drive/NtechLab_test/task2/testset')
-data_files = list(PATH_DATA.rglob('*.jpg'))[:50]
+  # Получаем предсказания с набора
+  dataset = GenderDataset(data_files, mode="test")
+  loader = DataLoader(dataset, shuffle=False, batch_size=64)
+  probs = predict(model, loader)
 
-# Получаем предсказания с набора
-dataset = GenderDataset(data_files, mode="test")
-loader = DataLoader(dataset, shuffle=False, batch_size=64)
-probs = predict(model, loader)
+  # Переводим вероятности классов в названия классов
+  label_encoder = LabelEncoder()
+  label_encoder.fit(["male", "female"])
 
-# Переводим вероятности классов в названия классов
-label_encoder = LabelEncoder()
-label_encoder.fit(["male", "female"])
+  # label_encoder = pickle.load(open("label_encoder.pkl", 'rb'))
+  preds = label_encoder.inverse_transform(np.argmax(probs, axis=1))
+  filenames = [path.name for path in dataset.files]
 
-# label_encoder = pickle.load(open("label_encoder.pkl", 'rb'))
-preds = label_encoder.inverse_transform(np.argmax(probs, axis=1))
-filenames = [path.name for path in dataset.files]
+  # Запись предсказаний в файл .json
+  df = pd.DataFrame()
+  df['Id'] = filenames
+  df['Expected'] = preds
 
-# Запись предсказаний в файл .json
-df = pd.DataFrame()
-df['Id'] = filenames
-df['Expected'] = preds
+  df.set_index('Id')['Expected'].to_json('process_results.json')
 
-df.set_index('Id')['Expected'].to_json('preds.json')
-
-print("Done")
+  print("Done")
